65cc96f98bf60a454560b682b68005684a334bf6
tommo593 <tomdevwork@gmail.com>
2024-11-24 12:17:46 +0000

    Added basic html button

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+echo"i like <br>borgirs"
+// this is not to be read
+/* this is not to be read over
+multiple lines
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button>order barraquito</button>
+</body>
+</html>
